71a2ca48ffe231f177f67de2590b4a012af6e68a
Mattias Pernhult <pernhultmattias@gmail.com>
2016-05-12 20:01:40 +0200

    Refactoring

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"sandbox/friends-api/source/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	setUpRoutes(router)
+	router.Run("localhost:8080")
+}
+
+func setUpRoutes(router *gin.Engine) {
+	router.GET("/", controllers.IndexController)
+	router.GET("/persons", controllers.PersonsController)
+	router.GET("/persons/:name", controllers.PersonController)
+	router.GET("/episodes", controllers.EpisodesController)
+}
